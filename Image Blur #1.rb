ecc3a53284ffbfd5f143b755cd0cd513fb10a76c
Cory Hack <cory.hack@gmail.com>
2018-01-15 16:34:27 +0000

    Add Image Blur #1 exercise

---
+++ Image Blur #1.rb
@@ -0,0 +1,18 @@
+class Image
+
+  def output_image
+    @images.each do |x|
+  puts x
+  end
+
+end
+
+
+image = Image.new([
+  [0, 0, 0, 0],
+  [0, 1, 0, 0],
+  [0, 0, 0, 1],
+  [0, 0, 0, 0]
+])
+
+image.output_image
